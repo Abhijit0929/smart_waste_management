a86205e3a10d2d409621197b5d3a46b86b7112af
Abhijit Dhumale <abhijitdhumale2909@gmail.com>
2026-07-30 02:27:22 +0530

    Configure headless Chrome for GitHub Actions

--- Automation/utils/driver_factory.py
+++ Automation/utils/driver_factory.py
@@ -1,12 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
 
 def get_driver():
 
-    options = webdriver.ChromeOptions()
+    options = Options()
 
+    if os.getenv("CI") == "true":
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+    else:
     options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(
